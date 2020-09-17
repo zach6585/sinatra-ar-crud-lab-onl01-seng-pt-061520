8101658377f1cb4af1a5f08951539f0a7548e653
zach6585 <zmines@wesleyan.edu>
2020-09-16 22:42:57 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,13 +24,46 @@ class ApplicationController < Sinatra::Base
     erb :index
   end 
   
-  # get '/articles/:id' do 
-  #   # binding.pry
-  #   a = params[:id]
-  # end 
+  get '/articles/new' do 
+    erb :new 
+  end 
+
+  get '/articles/:id' do 
+    # binding.pry
+    @article = Article.find(params[:id])
+    erb :show
+  end 
+
   
-  # post '/articles' do 
-  #   @article1 = Article.create(title: params[:article_title], content: params[:article_content])
-  #   binding.pry
-  # end 
+  post '/articles' do 
+    @article = Article.create(:title => params[:title], :content => params[:content])
+    redirect to "/articles/#{@article.id}"
+  end 
+
+  get '/articles/:id' do
+    @article = Article.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do 
+    # binding.pry
+    @article2 = Article.find_by_id(params[:id])
+    erb :edit
+  end 
+
+  patch '/articles/:id' do 
+    # binding.pry
+    @article = Article.find_by_id(params[:id])
+    @article.title= params[:title]
+    @article.content = params[:content] 
+    @article.save 
+    redirect to "/articles/#{@article.id}"
+  end 
+
+  delete '/articles/:id' do 
+    @article = Article.find_by_id(params[:id])
+    @article.delete
+    redirect to "/articles"
+  end 
+
 end
