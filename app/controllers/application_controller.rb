283e63f747aa38e645a57c96936359bcab3d02ef
zachary mines <zmines@wesleyan.edu>
2020-09-17 00:30:36 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 
 require_relative '../../config/environment'
+require_relative '../../app/models/article.rb'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -8,6 +10,27 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
-  end
+  # get '/' do
+    
+  # end
+  
+  get '/articles' do 
+    articles = Article.all 
+    @articles = []
+    articles.each do |article|
+      @articles << article.title
+    end 
+    @articles 
+    erb :index
+  end 
+  
+  # get '/articles/:id' do 
+  #   # binding.pry
+  #   a = params[:id]
+  # end 
+  
+  # post '/articles' do 
+  #   @article1 = Article.create(title: params[:article_title], content: params[:article_content])
+  #   binding.pry
+  # end 
 end
